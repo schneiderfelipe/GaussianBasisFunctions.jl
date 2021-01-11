43d61732ee31750fd951ed1707b17196a73aac53
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-01-11 17:28:53 -0300

    Separate overlap function

--- src/GaussianBasisFunctions.jl
+++ src/GaussianBasisFunctions.jl
@@ -4,12 +4,15 @@ using LinearAlgebra: Symmetric
 
 export Molecule
 export GaussianBasisFunction
+
 export build_sto3g
 export overlap
 
 include("types.jl")
 
 include("utils.jl")
-include("one_electron_integrals.jl")
+
+include("auxiliary.jl")
+include("overlap.jl")
 
 end
